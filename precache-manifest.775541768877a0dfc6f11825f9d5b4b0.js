31d313d1b48a40eaa924e2f280b420db742b86aa
Jason Park <parkjs814@gmail.com>
2019-03-30 01:34:33 -0400

    Updates

---
+++ precache-manifest.775541768877a0dfc6f11825f9d5b4b0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fd73d157453532e6665e5c3aa6be5c3",
+    "url": "/T-Support/static/media/logo-magenta-square.2fd73d15.png"
+  },
+  {
+    "revision": "90eb15794f4261a97254",
+    "url": "/T-Support/static/css/main.3f69c6a0.chunk.css"
+  },
+  {
+    "revision": "d0ed079d9a590c4b77a5",
+    "url": "/T-Support/static/js/runtime~main.d927c190.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/T-Support/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2a697d95ef24ee11aec0",
+    "url": "/T-Support/static/js/2.641a6899.chunk.js"
+  },
+  {
+    "revision": "3fdd39e506ac10ee50ba7f1cdb7035a5",
+    "url": "/T-Support/static/media/logo-white-square.3fdd39e5.png"
+  },
+  {
+    "revision": "90eb15794f4261a97254",
+    "url": "/T-Support/static/js/main.504db212.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/T-Support/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/T-Support/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/T-Support/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/T-Support/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "2a697d95ef24ee11aec0",
+    "url": "/T-Support/static/css/2.c9e875f3.chunk.css"
+  },
+  {
+    "revision": "66112975042a449c6eefa4f03625132e",
+    "url": "/T-Support/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.375780063279e26a5e4dbe1dc6d06fa1.js"
+  "/T-Support/precache-manifest.775541768877a0dfc6f11825f9d5b4b0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/T-Support/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
